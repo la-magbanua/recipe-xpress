53f49bc0dcc08f14f15fc255b32ef73928adf31e
la-magbanua <la.magbanua18@gmail.com>
2020-04-24 07:23:23 +0800

    added animate presence to ingredient items

--- src/components/ingredient-list/ingredient-list-styles.js
+++ src/components/ingredient-list/ingredient-list-styles.js
@@ -2,5 +2,4 @@ import styled from 'styled-components'
 
 export const StyledIngredientList = styled.div`
   margin: 16px 0 24px;
-  /* height: 30px; */
 `
--- src/components/ingredient-list/ingredient-list.js
+++ src/components/ingredient-list/ingredient-list.js
@@ -4,17 +4,20 @@ import { useRecipesState } from '../../contexts/recipes/recipes-context'
 import IngredientItem from '../ingredient-item/ingredient-item'
 
 import { StyledIngredientList } from './ingredient-list-styles'
+import { AnimatePresence } from 'framer-motion'
 
 const IngredientList = () => {
   const { ingredients } = useRecipesState()
 
   return (
     <StyledIngredientList>
-      {ingredients.length
-        ? ingredients.map((ingredient, i) => (
-            <IngredientItem key={i} ingredient={ingredient} />
-          ))
-        : null}
+      <AnimatePresence>
+        {ingredients.length
+          ? ingredients.map((ingredient, i) => (
+              <IngredientItem key={i} ingredient={ingredient} />
+            ))
+          : null}
+      </AnimatePresence>
     </StyledIngredientList>
   )
 }
